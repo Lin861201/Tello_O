68e79054efb7bc592bd14b07857e8b2ee356d86f
Lin861201 <43812826+Lin861201@users.noreply.github.com>
2019-10-13 10:00:51 -0700

    version_1

--- UDPClient.swift
+++ UDPClient.swift
@@ -1,24 +1,24 @@
 import Foundation
 
-@_silgen_name("tellosocket_server") func c_yudpsocket_server(_ host:UnsafePointer<Int8>,port:Int32) -> Int32
-@_silgen_name("tellosocket_recive") func c_yudpsocket_recive(_ fd:Int32,buff:UnsafePointer<Byte>,len:Int32,ip:UnsafePointer<Int8>,port:UnsafePointer<Int32>) -> Int32
-@_silgen_name("tellosocket_close") func c_yudpsocket_close(_ fd:Int32) -> Int32
-@_silgen_name("tellosocket_client") func c_yudpsocket_client() -> Int32
-@_silgen_name("tellosocket_get_server_ip") func c_yudpsocket_get_server_ip(_ host:UnsafePointer<Int8>,ip:UnsafePointer<Int8>) -> Int32
-@_silgen_name("tellosocket_sendto") func c_yudpsocket_sentto(_ fd:Int32,buff:UnsafePointer<Byte>,len:Int32,ip:UnsafePointer<Int8>,port:Int32) -> Int32
+@_silgen_name("tellosocket_server") func tello_socket_server(_ host:UnsafePointer<Int8>,port:Int32) -> Int32
+@_silgen_name("tellosocket_recive") func tello_socket_recive(_ fd:Int32,buff:UnsafePointer<Byte>,len:Int32,ip:UnsafePointer<Int8>,port:UnsafePointer<Int32>) -> Int32
+@_silgen_name("tellosocket_close") func tello_socket_close(_ fd:Int32) -> Int32
+@_silgen_name("tellosocket_client") func tello_socket_client() -> Int32
+@_silgen_name("tellosocket_get_server_ip") func tello_socket_get_server_ip(_ host:UnsafePointer<Int8>,ip:UnsafePointer<Int8>) -> Int32
+@_silgen_name("tellosocket_sendto") func tello_socket_sentto(_ fd:Int32,buff:UnsafePointer<Byte>,len:Int32,ip:UnsafePointer<Int8>,port:Int32) -> Int32
 
 open class UDPClient: Socket {
     public override init(address: String, port: Int32) {
         let remoteipbuff: [Int8] = [Int8](repeating: 0x0,count: 16)
-        let ret = c_yudpsocket_get_server_ip(address, ip: remoteipbuff)
+        let ret = tello_socket_get_server_ip(address, ip: remoteipbuff)
         guard let ip = String(cString: remoteipbuff, encoding: String.Encoding.utf8), ret == 0 else {
-            super.init(address: "", port: 0) //TODO: change to init?
+            super.init(address: "", port: 0)
             return
         }
 
         super.init(address: ip, port: port)
-        let fd: Int32 = c_yudpsocket_client()
-        //c_yudpsocket_server("192.168.43.36", port: 8787)
+        let fd: Int32 = tello_socket_client()
+
         if fd > 0 {
             self.fd = fd
         }
@@ -27,22 +27,16 @@ open class UDPClient: Socket {
     public convenience init(address: String, port: Int32, myAddresss: String, myPort: Int32) {
         self.init(address: address, port: port)
         
-        let fd: Int32 = c_yudpsocket_server(myAddresss, port: myPort)
+        let fd: Int32 = tello_socket_server(myAddresss, port: myPort)
         if fd > 0 {
             self.fd = fd
         }
     }
     
-
-    
-    /*
-    * send data
-    * return success or fail with message
-    */
     open func send(data: [Byte]) -> Result {
         guard let fd = self.fd else { return .failure(SocketError.connectionClosed) }
         
-        let sendsize: Int32 = c_yudpsocket_sentto(fd, buff: data, len: Int32(data.count), ip: self.address, port: Int32(self.port))
+        let sendsize: Int32 = tello_socket_sentto(fd, buff: data, len: Int32(data.count), ip: self.address, port: Int32(self.port))
         if Int(sendsize) == data.count {
             return .success
         } else {
@@ -50,14 +44,10 @@ open class UDPClient: Socket {
         }
     }
     
-    /*
-    * send string
-    * return success or fail with message
-    */
     open func send(string: String)-> Result {
         guard let fd = self.fd else { return .failure(SocketError.connectionClosed) }
         
-        let sendsize = c_yudpsocket_sentto(fd, buff: string, len: Int32(strlen(string)), ip: address, port: port)
+        let sendsize = tello_socket_sentto(fd, buff: string, len: Int32(strlen(string)), ip: address, port: port)
         if sendsize == Int32(strlen(string)) {
             return .success
         } else {
@@ -65,16 +55,12 @@ open class UDPClient: Socket {
         }
     }
     
-    /*
-    *
-    * send nsdata
-    */
     open func send(data: Data) -> Result {
         guard let fd = self.fd else { return .failure(SocketError.connectionClosed) }
         
         var buff = [Byte](repeating: 0x0,count: data.count)
         (data as NSData).getBytes(&buff, length: data.count)
-        let sendsize = c_yudpsocket_sentto(fd, buff: buff, len: Int32(data.count), ip: address, port: port)
+        let sendsize = tello_socket_sentto(fd, buff: buff, len: Int32(data.count), ip: address, port: port)
         if sendsize == Int32(data.count) {
             return .success
         } else {
@@ -82,7 +68,6 @@ open class UDPClient: Socket {
         }
     }
     
-    //TODO add multycast and boardcast
     open func recv(_ expectlen: Int) -> ([Byte]?, String, Int) {
         guard let fd = self.fd else {
             return (nil, "no ip", 0)
@@ -90,7 +75,7 @@ open class UDPClient: Socket {
         var buff: [Byte] = [Byte](repeating: 0x0, count: expectlen)
         var remoteipbuff: [Int8] = [Int8](repeating: 0x0, count: 16)
         var remoteport: Int32 = 0
-        let readLen: Int32 = c_yudpsocket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
+        let readLen: Int32 = tello_socket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
         let port: Int = Int(remoteport)
         let address = String(cString: remoteipbuff, encoding: String.Encoding.utf8) ?? ""
         
@@ -105,10 +90,9 @@ open class UDPClient: Socket {
     open func close() {
         guard let fd = self.fd else { return }
         
-        _ = c_yudpsocket_close(fd)
+        _ = tello_socket_close(fd)
         self.fd = nil
     }
-    //TODO add multycast and boardcast
 }
 
 open class UDPServer: Socket {
@@ -116,19 +100,18 @@ open class UDPServer: Socket {
     public override init(address: String, port: Int32) {
         super.init(address: address, port: port)
       
-        let fd = c_yudpsocket_server(address, port: port)
+        let fd = tello_socket_server(address, port: port)
         if fd > 0 { 
             self.fd = fd
         }
     }
   
-    //TODO add multycast and boardcast
     open func recv(_ expectlen: Int) -> ([Byte]?, String, Int) {
         if let fd = self.fd {
             var buff: [Byte] = [Byte](repeating: 0x0,count: expectlen)
             var remoteipbuff: [Int8] = [Int8](repeating: 0x0,count: 16)
             var remoteport: Int32 = 0
-            let readLen: Int32 = c_yudpsocket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
+            let readLen: Int32 = tello_socket_recive(fd, buff: buff, len: Int32(expectlen), ip: &remoteipbuff, port: &remoteport)
             let port: Int = Int(remoteport)
             var address = ""
             if let ip = String(cString: remoteipbuff, encoding: String.Encoding.utf8) {
@@ -148,9 +131,10 @@ open class UDPServer: Socket {
     }
   
     open func close() {
-        guard let fd = self.fd else { return }
         
-        _ = c_yudpsocket_close(fd)
+        guard let fd = self.fd else { return }
+        _ = tello_socket_close(fd)
         self.fd = nil
+        
     }
 }
